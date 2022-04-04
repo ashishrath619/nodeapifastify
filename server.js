dca5f1c674f292522b15290dcb6e15b5c817cabf
ashishrath619 <75829394+ashishrath619@users.noreply.github.com>
2022-04-04 10:37:41 +0530

    initial

--- server.js
+++ server.js
@@ -5,9 +5,25 @@ const routes = require("./routes/userRouter");
 
 fastify.register(routes);
 
+// const app = Fastify({
+//   logger: true,
+//   pluginTimeout: 10000,
+// });
+
+// app.listen(process.env.PORT || 3000, "0.0.0.0", (err) => {
+//   if (err) {
+//     app.log.error(err);
+//     process.exit(1);
+//   }
+// });
 const start = async () => {
   try {
-    await fastify.listen(PORT);
+    await fastify.listen(3000, "0.0.0.0", (err) => {
+      if (err) {
+        app.log.error(err);
+        process.exit(1);
+      }
+    });
   } catch (error) {
     fastify.log.error(error);
     process.exit(1);
